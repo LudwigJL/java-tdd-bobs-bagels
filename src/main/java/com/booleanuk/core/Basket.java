d9ed0dc38f67ded7e38278dc9923229adcb58c9d
Ludwig J Lundborg <ludwig.j.lundborg@gmail.com>
2024-08-14 15:18:01 +0200

    passing green tests for add bagel and remove bagel

--- src/main/java/com/booleanuk/core/Basket.java
+++ src/main/java/com/booleanuk/core/Basket.java
@@ -1,6 +1,37 @@
 package com.booleanuk.core;
 
+import java.util.ArrayList;
+
 public class Basket {
-    
+
+    private ArrayList<String> bagels;
+    private int basketLimit = 2;
+    private int numberOfBaskets = 10;
+
+    public Basket() {
+        this.bagels = new ArrayList<>();
+
+    }
+
+    public boolean addBagel(String bagelType) {
+        if (bagels.size() < basketLimit) {
+            bagels.add(bagelType);
+
+            return true;
+        }
+        return false;
+    }
+
+
+    public boolean removeBagel(String bagelType) {
+        if (bagels.contains(bagelType)){
+            bagels.remove(bagelType);
+
+            return true;
+        }
+        return false;
+    }
+
+
 
 }
